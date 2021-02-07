966aed715f683f410ca65955bed8da54eb1f903a
Trevor Keegan <keegant@uwplatt.edu>
2021-02-06 23:20:17 -0600

    test

--- hello_world.lua
+++ hello_world.lua
@@ -1,12 +1,32 @@
-while turtle.detect() do
-  turtle.up()
- end
- turtle.down()
- turtle.back()
- turtle.place()
- while not turtle.detectDown() do
-  turtle.back()
-  turtle.down()
-  turtle.place()
- end
- print("Staircase Built")
+turtle.refuel()
+
+function left_strafe()
+
+  turtle.turnLeft()
+  turtle.forward()
+  turtle.turnRight() 
+  
+end
+
+function right_strafe()
+
+  turtle.turnRight()
+  turtle.forward()
+  turtle.turnLeft() 
+  
+end
+
+
+local index = 0
+
+while(index < 5) do
+  
+  turtle.dig()
+  turtle.turnRight()
+  turtle.dig()
+  turtle.digDown()
+  turtle.forward()
+  turtle.digDown()
+
+  index = index + 1
+end
