e4c625879822be902293beae02e8e7b5d36bdcb4
Trevor Keegan <keegant@uwplatt.edu>
2021-02-06 22:07:10 -0600

    test

--- hello_world.lua
+++ hello_world.lua
@@ -1,2 +1,12 @@
-local hi = 1
-print("HELLO")
+while turtle.detect() do
+  turtle.up()
+ end
+ turtle.down()
+ turtle.back()
+ turtle.place()
+ while not turtle.detectDown() do
+  turtle.back()
+  turtle.down()
+  turtle.place()
+ end
+ print("Staircase Built")
